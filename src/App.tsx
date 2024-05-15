5ad0cad255408340177d73b6c6886b570d7ef5b3
honganji <85441887+honganji@users.noreply.github.com>
2024-05-15 20:06:46 +0200

    modify App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -7,13 +7,15 @@ import Page2 from "./pages/page2/page2";
 function App() {
   return (
    <div>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home/>}/>
-        <Route path="/page1" element={<Page1/>}/>
-        <Route path="/page2" element={<Page2/>}/>
-      </Routes>
-    </BrowserRouter>
+    <div className="flex h-screen flex-col">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/page1" element={<Page1/>}/>
+          <Route path="/page2" element={<Page2/>}/>
+        </Routes>
+      </BrowserRouter>
+    </div>
    </div>
   );  
 }
